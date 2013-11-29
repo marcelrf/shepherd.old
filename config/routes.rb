daa610fd0aaaf4dbfed8c07fa0095f4c0e76e180
Marcel Ruiz Forns <marcel@chaordicsystems.com>
2013-11-29 09:57:41 -0200

    first heatmap version

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,9 @@
 Shepherd::Application.routes.draw do
+  root :to => 'maps#show'
 
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   
   resources :observations
   resources :metrics
-
-  match 'tasks/analyze-metric' => 'tasks#analyze_metric', :as => :analyze_metric_task
-
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
 end
