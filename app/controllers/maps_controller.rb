ff61fc8b1537c4d3f97d98b452339d46d1077750
Marcel Ruiz Forns <marcel@chaordicsystems.com>
2013-12-30 17:36:51 -0200

    added support for map filtering and grouping

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,7 +1,14 @@
 class MapsController < InheritedResources::Base
   def show
-    @metric_infos = Metric.all.map do |metric|
-      [metric, metric.state(params[:period])]
+    @filter = params[:filter]
+    @group = params[:group]
+    metrics = Metric.where("name like ?", "%#{@filter}%")
+    @metric_infos = Hash.new{|h, k| h[k] = []}
+    metrics.each do |metric|
+      match = /#{@group}/.match(metric.name)
+      group = match ? match[1] : nil
+      metric_info = [metric, metric.state(params[:period])]
+      @metric_infos[group].push(metric_info)
     end
   end
 end
