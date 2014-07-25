03f977fbb2411d56dba5d61ce756f7e34bffba6e
Marcel Ruiz Forns <marcelrf@gmail.com>
2014-07-25 01:52:39 -0300

    refactored metrics and observations (adding sources)

--- app/models/metric.rb
+++ app/models/metric.rb
@@ -1,26 +1,11 @@
 class Metric < ActiveRecord::Base
-  attr_accessible :name, :source_info, :polarity, :check_every, :check_delay, :kind
+  attr_accessible :name, :polarity, :kind
   has_many :observations
+  belongs_to :source
 
   validates :name, :presence => true, :length => { :minimum => 2 }
   validates :polarity, :presence => true, inclusion: { :in =>  %w(positive negative) }
   validates :kind, :presence => true, inclusion: { :in =>  %w(counter gauge) }
-  validates :check_every, :presence => true, inclusion: { :in =>  %w(hour day week month) }
-  validates :check_delay, :numericality => { :only_integer => true, :greater_than_or_equal_to => 0}, :allow_nil => true
-
-  def get_source_info
-    JSON.load(self['source_info'])
-  end
-
-  def set_source_info(source_info)
-    self['source_info'] = JSON.dump(source_info)
-  end
-
-  def periods
-    all_periods = ['hour', 'day', 'week', 'month']
-    metric_period_index = all_periods.index(self['check_every'])
-    all_periods[metric_period_index..-1]
-  end
 
   def state(period)
     observations = Observation.where(:metric_id => self['id'], :period => period)
