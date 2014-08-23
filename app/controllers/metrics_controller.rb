e360e9b974f7b3052c38fe4c2b6b89211bbe21be
Marcel Ruiz Forns <marcelrf@gmail.com>
2014-08-22 22:31:09 -0300

    added autocomplete (server side)

--- app/controllers/metrics_controller.rb
+++ app/controllers/metrics_controller.rb
@@ -1,2 +1,11 @@
 class MetricsController < InheritedResources::Base
+  def autocomplete
+    source = Source.find(params['source'].to_i)
+    metrics = SourceData.get_metrics(source, params['name'])
+    render :json => {
+        'source' => params['source'],
+        'name' => params['name'],
+        'metrics' => metrics
+    }
+  end
 end
