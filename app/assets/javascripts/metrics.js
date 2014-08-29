d2f18b69b47a793213d092b591bbbd325e2c5503
Marcel Ruiz Forns <marcelrf@gmail.com>
2014-08-29 11:03:46 -0300

    implements submitting of various metrics at the same time

--- app/assets/javascripts/metrics.js
+++ app/assets/javascripts/metrics.js
@@ -12,20 +12,9 @@ var Metrics = (function () {
         loading.html('<img src="http://pinpopular.in/images/spinner_192.gif" />');
         $.get(url, function (data) {
             loading.html('');
-            generateNameList(data.metrics);
             button.prop("disabled", false);
+            $("#metric-list").html(data.metrics);
         });
-    };
-
-    var generateNameList = function (metrics) {
-
-//  <div class="control-group">
-//    <div class="controls">
-//      <input name="metric[name]" type="hidden" value="0"><input id="metric_name" name="metric[name]" type="checkbox" value="1"> Positive
-//    </div>
-//  </div>
-
-        console.log(metrics);
     };
 
     return {
