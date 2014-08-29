18c3220f3f8367aea391809ed36707490149ccad
Marcel Ruiz Forns <marcelrf@gmail.com>
2014-08-29 16:26:21 -0300

    neglects day period, only hour

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Shepherd::Application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
-  match '/maps/:period' => 'maps#show'
+  match '/maps' => 'maps#show', :period => 'hour'
   match '/metrics/autocomplete' => 'metrics#autocomplete'
   
   resources :sources
