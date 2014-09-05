c35167a595a3aa5068a579ad6220c0f7b3905129
Marcel Ruiz Forns <marcelrf@gmail.com>
2014-09-05 16:10:00 -0300

    adds alerts crud functionalities

--- app/controllers/alerts_controller.rb
+++ app/controllers/alerts_controller.rb
@@ -1,2 +1,48 @@
 class AlertsController < InheritedResources::Base
+  def autocomplete
+    metrics = Metric.where("name LIKE '%#{params[:name]}%'").map do |metric|
+        metric.name
+    end
+    metrics_html = render_to_string(
+        :partial => 'metrics/autocomplete',
+        :layout => false,
+        :locals => {:metrics => metrics}
+    )
+    render :json => {
+        'source' => params['source'],
+        'name' => params['name'],
+        'metrics' => metrics_html
+    }
+  end
+
+  def create
+    if params['metric_list']
+        ActiveRecord::Base.transaction do
+            params['metric_list'].each do |metric_name|
+                params[:alert][:metric_name] = metric_name
+                Alert.create!(params[:alert])
+            end
+        end
+        flash[:notice] = "Alerts were successfully created."
+        redirect_to alerts_path
+    else
+        @alert = Alert.new(params[:metric])
+        if @alert.save
+          flash[:notice] = "Alert was successfully created."
+          redirect_to alerts_path
+        else
+          render "new"
+        end
+    end
+  end
+
+  def update
+    @alert = Alert.find(params[:id])
+    if @alert.update_attributes(params[:alert])
+      flash[:notice] = "Alert was successfully updated."
+      redirect_to alerts_path
+    else
+      render "edit"
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,12 @@ Shepherd::Application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
-  match '/maps' => 'maps#show', :period => 'hour'
-  match '/', :to => redirect('/maps')
+  match '/status' => 'maps#show', :period => 'hour'
+  match '/', :to => redirect('/status')
   match '/metrics/autocomplete' => 'metrics#autocomplete'
+  match '/alerts/autocomplete' => 'alerts#autocomplete'
   
   resources :sources
   resources :metrics
-  resources :observations
+  resources :alerts
 end
