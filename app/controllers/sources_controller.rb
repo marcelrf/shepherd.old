9cef43dff8e9440649f3f71bd3c0cb30a6ea43d7
Marcel Ruiz Forns <marcelrf@gmail.com>
2014-09-05 16:10:28 -0300

    modifies menu appearance and crud flow

--- app/controllers/sources_controller.rb
+++ app/controllers/sources_controller.rb
@@ -1,2 +1,21 @@
 class SourcesController < InheritedResources::Base
+  def create
+    @source = Source.new(params[:source])
+    if @source.save
+      flash[:notice] = "Source was successfully created."
+      redirect_to sources_path
+    else
+      render "new"
+    end
+  end
+
+  def update
+    @source = Source.find(params[:id])
+    if @source.update_attributes(params[:source])
+      flash[:notice] = "Source was successfully updated."
+      redirect_to sources_path
+    else
+      render "edit"
+    end
+  end
 end
