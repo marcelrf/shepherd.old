7a4a54d255dec65b257e17739dd3665e4ed1827d
Marcel Ruiz Forns <marcelrf@gmail.com>
2014-09-06 03:12:32 -0300

    adds autoreload and autoresize of the map

--- app/helpers/maps_helper.rb
+++ app/helpers/maps_helper.rb
@@ -1,12 +1,4 @@
 module MapsHelper
-  def total_width
-    700
-  end
-  
-  def total_height
-    500
-  end
-
   def table_measures(width, height, count)
     columns, rows = 1, 1
     column_width, row_height = width, height
@@ -22,32 +14,32 @@ module MapsHelper
     [columns, rows, column_width, row_height]
   end
 
-  def cell_color(metric, state)
+  def cell_color(metric, divergence)
     if !metric
       "#FFFFFF"
-    elsif !state
+    elsif !divergence
       "#F0F0F0"
-    elsif state.divergence <= 1 && state.divergence >= -1
+    elsif divergence <= 1 && divergence >= -1
       "#FFFFFF"
-    elsif state.divergence > 1
-      red_blue = (255 - (-1 / (((state.divergence - 1) / 3) ** 2 + 1) + 1) * 255).to_i.to_s(16)
+    elsif divergence > 1
+      red_blue = (255 - (-1 / (((divergence - 1) / 3) ** 2 + 1) + 1) * 255).to_i.to_s(16)
       red_blue = "0#{red_blue}" if red_blue.size == 1
       "##{red_blue}FF#{red_blue}"
     else
-      green_blue = (255 - (-1 / (((-state.divergence - 1) / 3) ** 2 + 1) + 1) * 255).to_i.to_s(16)
+      green_blue = (255 - (-1 / (((-divergence - 1) / 3) ** 2 + 1) + 1) * 255).to_i.to_s(16)
       green_blue = "0#{green_blue}" if green_blue.size == 1
       "#FF#{green_blue}#{green_blue}"
     end
   end
 
-  def format_divergence(metric, state)
+  def format_divergence(metric, divergence)
     if !metric
       nil
-    elsif state
-      if state.divergence >= 0
-        "+#{state.divergence}"
+    elsif divergence
+      if divergence >= 0
+        "+#{divergence}"
       else
-        "#{state.divergence}"
+        "#{divergence}"
       end
     else
       "Still no data"
@@ -55,6 +47,6 @@ module MapsHelper
   end
 
   def chart_url(metric)
-      "https://metrics.librato.com/metrics/#{metric.name}?duration=604800"
+      "https://metrics.librato.com/metrics/#{metric.name}?duration=864000"
   end
 end
